c9cb62e8434fd076b5a1b9fa2c466cf2fb324b8b
Vikas <khativikas97@gmail.com>
2023-02-22 03:27:47 +0530

    learning

--- calc.js
+++ calc.js
@@ -6,6 +6,20 @@ let divide = (a,b) => a/b ;
 
 let operate = (func, a,b) => func(a,b);
 
-console.log(operate(sum, 4,5));
+// creating dictionary of buttons
+ 
+let buttons = {};
 
+for(i = 0; i < 10; i++){
+    buttons["button-" +i] = document.getElementById("button-"+i) ;
+}
 
+// https://dev.to/smpnjn/javascript-add-event-listener-to-multiple-elements-2jah -- read this link
+let elements = document.querySelectorAll('.button');
+
+let clickEvent = () => {
+    console.log('some event content here...')
+}
+elements.forEach((item) => {
+    item.addEventListener('click', clickEvent)
+});
