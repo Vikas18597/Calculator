5e6f9479146a0e0cd3d168cad4461a2131197ac3
Vikas <khativikas97@gmail.com>
2023-02-27 14:14:28 +0530

    Using eslint to make changes

--- calc.js
+++ calc.js
@@ -1,91 +1,68 @@
-// all the functions are here 
+// all the functions are here
 
-let sum = (a,b) => a+b ;
-let subtract = (a,b) => a - b; 
-let multiply = (a,b) => a*b; 
-let divide = (a,b) =>(a/b).toFixed(2) ; 
+const sum = (a, b) => a + b
+const subtract = (a, b) => a - b
+const multiply = (a, b) => a * b
+const divide = (a, b) => (a / b).toFixed(2)
 
 // let operate = (func, a,b) => func(a,b);
 
-function operate(operand,a,b){
-  switch (operand){
-    case '+': return sum(a,b);
-    case '-': return subtract(a,b);
-    case '/': return divide(a,b);
-    case '*': return multiply(a,b);
+function operate (operand, a, b) {
+  switch (operand) {
+    case '+': return sum(a, b)
+    case '-': return subtract(a, b)
+    case '/': return divide(a, b)
+    case '*': return multiply(a, b)
   }
 }
 
-// function to describe what to do in case of equal sign is pressed 
-function equal(val) {
-  let expression = val.innerText.split(/\+|\-|\/|\*/) ; // regex code for seperating number from operator 
-  let first_num = expression[0], second_num = expression[1]; // associating numbers to variables 
-  console.log("The first number is " + first_num); 
-  console.log("The second number is " + second_num);
-  val.textContent = operate(current_operand, Number(first_num) ,Number(second_num)) ; 
+// function to describe what to do in case of equal sign is pressed
+function equal (val) {
+  const expression = val.innerText.split(/\+|-|\/|\*/) // regex code for seperating number from operator
+  const firstNum = expression[0]
+  const secondNum = expression[1] // associating numbers to variables
+  console.log('The first number is ' + firstNum)
+  console.log('The second number is ' + secondNum)
+  val.textContent = operate(currentOperand, Number(firstNum), Number(secondNum))
   return val.textContent
 }
 
-// using event delegation to call Event listener only one time 
-let current_operand = null; 
-let numbers = document.querySelector('.numbers');
-let val = document.querySelector('.display');
+// using event delegation to call Event listener only one time
+let currentOperand = null
+const numbers = document.querySelector('.numbers')
+const val = document.querySelector('.display')
 
+const clicked = (x) => {
+  if (x.target.tagName === 'BUTTON') {
+    const currentVal = x.target.innerText
 
-let clicked = (x) => {
-    if(x.target.tagName === 'BUTTON') {
+    // will delete the last element
+    if (currentVal === 'C') {
+      val.textContent = val.textContent.slice(0, -1)
+    } else if (currentVal === '+' || currentVal === '-' || currentVal === '*' || currentVal === '/') {
+      const arr = val.innerText.split(/\+|-|\/|\*/)
+      console.log(arr)
 
-      let current_val = x.target.innerText ; 
-
-      
-      // will delete the last element  
-      if (current_val == 'C'){
-        val.textContent  = val.textContent.slice(0,-1) ; 
+      if (arr.length >= 2) {
+        val.textContent = equal(val)
       }
 
-      // will show addition
-      else if (current_val == '+' || current_val == '-' || current_val == '*' || current_val == '/') {
-        let arr = val.innerText.split(/\+|\-|\/|\*/);
-        console.log(arr);
-  
-        if (arr.length >= 2){
-          val.textContent = equal(val);
-
-        }
-
-        current_operand = current_val;
-        val.textContent  +=  x.target.textContent;
-      }
-
-      // equal operand 
-      else if (current_val == '='){
-        val.textContent = equal(val);
-      }
-
-      // will insert the number
-      else{
-        val.textContent += current_val ;
-      } 
-      
+      currentOperand = currentVal
+      val.textContent += x.target.textContent
+    } else if (currentVal === '=') { // equal operand
+      val.textContent = equal(val)
+    } else { // will insert the number
+      val.textContent += currentVal
     }
-     
-  }; 
+  }
+}
 
-numbers.addEventListener("click", clicked ); 
+numbers.addEventListener('click', clicked)
 
 // // calling a function directly -- not efficient because event listener is called multiple times
-// let elements = document.querySelectorAll('button'); 
-// let clicked = (x) => { return console.log(x.innerHTML)} ;  
+// let elements = document.querySelectorAll('button');
+// let clicked = (x) => { return console.log(x.innerHTML)} ;
 
-// elements.forEach(item => { 
+// elements.forEach(item => {
 //     item.addEventListener('click', () => {clicked(item);}) ;
 // })
-
-
-
-
-
-
-
-
-
